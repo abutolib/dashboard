415b6f58d9701bfeb388c2ad27a78c0897af1afe
abutolib <abutolib12042002@gmail.com>
2023-02-18 20:41:45 +0500

    some changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,21 +8,19 @@ export const Navbar = () => {
 
   return (
     <nav className='navbar'>
-      
-        <div className='container1'>
-        <div className='navbar-header'>
-          <a className='navbar-header__link' href='#'>
-            <img src={Logo} />
-            <img src={Logo2}/>
-          </a>
-        </div>
-        </div>
-        <nav className='navbar__links'>
-          <ul className='navbar__list'>
-            <NavbarItem/>
-          </ul>
-        </nav>
-      
+      <div className='navbar-header'>
+        <a className='navbar-header__link' href='#'>
+          <img src={Logo} />
+          <img src={Logo2} />
+        </a>
+      </div>
+
+      <nav className='navbar__links'>
+        <ul className='navbar__list'>
+          <NavbarItem />
+        </ul>
+      </nav>
+
     </nav>
   )
 }
